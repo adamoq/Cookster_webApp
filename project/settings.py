f34309aa086ce7e209c5f5d57084b7554af76e8b
Ferchichi <adam.ferchichi@accenture.com>
2018-05-28 12:33:15 +0200

    ant conf

--- project/settings.py
+++ project/settings.py
@@ -138,8 +138,9 @@ INTERNAL_IPS = ['127.0.0.1']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+STATIC_URL = '/media/'
+STATIC_ROOT = os.path.join(ENV_PATH, 'media/')
