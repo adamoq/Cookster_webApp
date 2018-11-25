97fb0fcd4e809b8cf4d8eed54cd53bf243fd4233
Adam Ferchichi <adamoq@gmail.com>
2018-11-25 19:13:26 +0000

    deplo ver.

--- cook/api.py
+++ cook/api.py
@@ -47,20 +47,6 @@ class EmployeeResource(ModelResource):
         }
 
 class CookTaskResource(ModelResource):
-	pass
-class OrderCookResource(ModelResource):
-	product = fields.ForeignKey(ProductResource, 'product',full=True)
-	task = fields.ForeignKey(CookTaskResource, 'task',full=True,null=True)
-	class Meta:
-		always_return_data = True
-		queryset = CookOrder.objects.all()
-		resource_name = 'cookorders'
-		allowed_methods = ['get','put', 'post', 'delete']
-		authentication = Authentication()
-		authorization = Authorization()
-		
-		
-class CookTaskResource(ModelResource):
 	provider = fields.ForeignKey(EmployeeResource, 'provider',full=True)
 	cook = fields.ForeignKey(EmployeeResource, 'cook',full=True)
 	#orders = fields.ManyToManyField(OrderCookResource, 'cookorders',full=True, null = True)
@@ -71,7 +57,7 @@ class CookTaskResource(ModelResource):
 		today_minm = datetime.date.today().strftime("%m")
 		today_mind = datetime.date.today().strftime("%d")
 		queryset = CookTask.objects.filter(created_at__year = today_min, created_at__month = today_minm, created_at__day = today_mind)
-	
+
 		resource_name = 'cooktasks'
 		allowed_methods = ['get','put', 'post', 'delete']
 		authentication = Authentication()
@@ -79,8 +65,21 @@ class CookTaskResource(ModelResource):
 	def dehydrate(self, bundle):
 		bundle.data['orders'] = CookOrder.objects.filter(task = bundle.data['id']).values('count', 'product__name', 'product__unit')
 		return bundle
-	def orders(self, instance):
-		return instance['product']['name']
+
+
+class OrderCookResource(ModelResource):
+	product = fields.ForeignKey(ProductResource, 'product',full=True)
+	task = fields.ForeignKey(CookTaskResource, 'task',full=True,null=True)
+	class Meta:
+		always_return_data = True
+		queryset = CookOrder.objects.all()
+		resource_name = 'cookorders'
+		allowed_methods = ['get','put', 'post', 'delete']
+		authentication = Authentication()
+		authorization = Authorization()
+
+
+
 
 
 class CurrencyResource(ModelResource):
@@ -113,7 +112,7 @@ class OrderResource(ModelResource):
 		authentication = Authentication()
 		authorization = Authorization()
 		allowed_methods = ['get','put', 'post', 'delete']
-		
+
 		filtering = {
             'state': ALL,
             'waiter': ALL_WITH_RELATIONS,
